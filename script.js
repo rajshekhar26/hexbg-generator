88f83485eb3325889df0444c99af461804421783
Raj Shekhar <67941012+rajshekhar26@users.noreply.github.com>
2021-02-03 13:51:30 +0530

    Add button transform when clicked

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const button = document.getElementById("button");
-const displayColor = document.getElementById("display-color");
+const button = document.getElementById('button');
+const displayColor = document.getElementById('display-color');
 const body = document.body;
-hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"]
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
 const changeBackground = () => {
-    let bgcolor = "#"
-    for (let i = 0; i < 6; i++) {
-        let index = Math.floor(Math.random() * hex.length);
-        bgcolor += hex[index]
-    }
-    body.style.backgroundColor = bgcolor;
-    displayColor.textContent = `Color - ${bgcolor}`;
-}
+	let bgcolor = '#';
+	for (let i = 0; i < 6; i++) {
+		let index = Math.floor(Math.random() * hex.length);
+		bgcolor += hex[index];
+	}
+	body.style.backgroundColor = bgcolor;
+	button.style.color = bgcolor;
+	displayColor.textContent = `COLOR - ${bgcolor}`;
+};
 
-button.addEventListener("click", changeBackground)
+button.addEventListener('click', changeBackground);
